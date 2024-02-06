0221441f4767e0f9bde2228c23e7602b18bb4540
Andy Shapiro <shapiromatron@gmail.com>
2024-02-06 14:22:03 -0500

    standardize server error parsing

--- frontend/src/components/Main/AnalysisForm/AnalysisForm.js
+++ frontend/src/components/Main/AnalysisForm/AnalysisForm.js
@@ -2,8 +2,8 @@ import {inject, observer} from "mobx-react";
 import PropTypes from "prop-types";
 import React, {Component} from "react";
 
-import {parseError} from "../../../utils/parsers";
 import Button from "../../common/Button";
+import ErrorMessage from "../../common/ErrorMessage";
 import Icon from "../../common/Icon";
 import SelectInput from "../../common/SelectInput";
 import Spinner from "../../common/Spinner";
@@ -64,12 +64,7 @@ class AnalysisForm extends Component {
                             choices={mainStore.getModelTypeChoices}
                         />
                     </div>
-
-                    {mainStore.errorMessage ? (
-                        <div className="alert alert-danger">
-                            {parseError(mainStore.errorMessage)}
-                        </div>
-                    ) : null}
+                    <ErrorMessage error={mainStore.errorMessage} />
                     <div id="controlPanel" className="card bg-light">
                         {mainStore.isExecuting ? (
                             <div className="card-body">
--- frontend/tests/utils/parsers.test.js
+++ frontend/tests/utils/parsers.test.js
@@ -1,20 +1,147 @@
-import { parseError } from "../../src/utils/parsers";
+import {
+    extractErrorFromTraceback,
+    parsePydanticError,
+    parseServerErrors,
+} from "../../src/utils/parsers";
 import assert from "../helpers";
 
-const errors = [
-    [
-        'Traceback (most recent call last): File "/usr/local/lib/python3.11/site-packages/bmds_server/analysis/models.py", line 237, in try_run_session return AnalysisSession.run(inputs, dataset_index, option_index) ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ File "/usr/local/lib/python3.11/site-packages/bmds_server/analysis/executor.py", line 134, in run session = cls.create(inputs, dataset_index, option_index) ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ File "/usr/local/lib/python3.11/site-packages/bmds_server/analysis/executor.py", line 140, in create dataset = build_dataset(inputs["datasets"][dataset_index]) ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ File "/usr/local/lib/python3.11/site-packages/bmds_server/analysis/transforms.py", line 93, in build_dataset return schema.model_validate(dataset).deserialize() ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ File "/usr/local/lib/python3.11/site-packages/bmds/datasets/continuous.py", line 247, in deserialize ds = ContinuousDataset( ^^^^^^^^^^^^^^^^^^ File "/usr/local/lib/python3.11/site-packages/bmds/datasets/continuous.py", line 104, in _init_ self._sort_by_dose_group() File "/usr/local/lib/python3.11/site-packages/bmds/datasets/continuous.py", line 116, in _sort_by_dose_group self._validate() File "/usr/local/lib/python3.11/site-packages/bmds/datasets/continuous.py", line 108, in _validate self._validate_summary_data() File "/usr/local/lib/python3.11/site-packages/bmds/datasets/continuous.py", line 23, in _validate_summary_data raise ValueError("Doses are not unique") ValueError: Doses are not unique',
-        "Doses are not unique"
-    ]
-];
+describe("Parsing", function() {
+    describe("parseServerErrors", function() {
+        it("handles no error correct", function() {
+            assert.equal(parseServerErrors(""), null);
+            assert.equal(parseServerErrors(null), null);
+            assert.equal(parseServerErrors(undefined), null);
+            assert.equal(parseServerErrors([]), null);
+        });
 
-describe("Parsing", function () {
-    describe("parseError", function () {
-        it("extracts the correct error message", function () {
-            errors.map(args => {
+        it("handles an unknown format", function() {
+            const expected = {
+                messages: ["An error has occurred"],
+                message: "An error has occurred",
+            };
+
+            expected.data = ["ERROR"];
+            assert.deepStrictEqual(parseServerErrors("ERROR"), expected);
+
+            expected.data = [["ERROR"]];
+            assert.deepStrictEqual(parseServerErrors(["ERROR"]), expected);
+
+            expected.data = [{err: "ERROR"}];
+            assert.deepStrictEqual(parseServerErrors({err: "ERROR"}), expected);
+
+            expected.data = [[{err: "ERROR"}]];
+            assert.deepStrictEqual(parseServerErrors([{err: "ERROR"}]), expected);
+        });
+
+        it("handles tracebacks", function() {
+            assert.deepStrictEqual(
+                parseServerErrors([
+                    'Traceback (most recent call last):\n  File "/bmds-server/bmds_server/analysis/models.py", line 246, in try_run_session\n    return AnalysisSession.run(inputs, dataset_index, option_index)\nValueError: Doses are not unique\n',
+                ]),
+                {
+                    data: [
+                        'Traceback (most recent call last):\n  File "/bmds-server/bmds_server/analysis/models.py", line 246, in try_run_session\n    return AnalysisSession.run(inputs, dataset_index, option_index)\nValueError: Doses are not unique\n',
+                    ],
+                    messages: ["Doses are not unique"],
+                    message: "Doses are not unique",
+                }
+            );
+        });
+
+        it("handles pydantic", function() {
+            assert.deepStrictEqual(
+                parseServerErrors([
+                    '[{"type":"float_type","loc":["datasets",0,"function-after[num_groups(), MaxContinuousDatasetSchema]","doses",1],"msg":"Input should be a valid number","url":"https://errors.pydantic.dev/2.4/v/float_type"},{"type":"float_type","loc":["datasets",0,"function-after[num_groups(), MaxContinuousIndividualDatasetSchema]","doses",1],"msg":"Input should be a valid number","url":"https://errors.pydantic.dev/2.4/v/float_type"},{"type":"missing","loc":["datasets",0,"function-after[num_groups(), MaxContinuousIndividualDatasetSchema]","responses"],"msg":"Field required","url":"https://errors.pydantic.dev/2.4/v/missing"}]',
+                ]),
+                {
+                    data: [
+                        {
+                            loc: [
+                                "datasets",
+                                0,
+                                "function-after[num_groups(), MaxContinuousDatasetSchema]",
+                                "doses",
+                                1,
+                            ],
+                            msg: "Input should be a valid number",
+                            type: "float_type",
+                            url: "https://errors.pydantic.dev/2.4/v/float_type",
+                        },
+                        {
+                            loc: [
+                                "datasets",
+                                0,
+                                "function-after[num_groups(), MaxContinuousIndividualDatasetSchema]",
+                                "doses",
+                                1,
+                            ],
+                            msg: "Input should be a valid number",
+                            type: "float_type",
+                            url: "https://errors.pydantic.dev/2.4/v/float_type",
+                        },
+                        {
+                            loc: [
+                                "datasets",
+                                0,
+                                "function-after[num_groups(), MaxContinuousIndividualDatasetSchema]",
+                                "responses",
+                            ],
+                            msg: "Field required",
+                            type: "missing",
+                            url: "https://errors.pydantic.dev/2.4/v/missing",
+                        },
+                    ],
+                    messages: [
+                        "datasets: Field required",
+                        "datasets: Input should be a valid number",
+                        "datasets: Input should be a valid number",
+                    ],
+                    message: "datasets: Field required\ndatasets: Input should be a valid number",
+                }
+            );
+        });
+    });
+
+    describe("extractErrorFromTraceback", function() {
+        it("extracts the error from a python traceback", function() {
+            const tracebackErrors = [
+                [
+                    'Traceback (most recent call last):\n  File "/bmds-server/bmds_server/analysis/models.py", line 246, in try_run_session\n    return AnalysisSession.run(inputs, dataset_index, option_index)\nValueError: Doses are not unique\n',
+                    "Doses are not unique",
+                ],
+                [
+                    'Traceback (most recent call last):\n  File "/bmds-server/bmds_server/analysis/models.py", line 246, in try_run_session\n    return AnalysisSession.run(inputs, dataset_index, option_index)\nDoses are not unique\n',
+                    "Doses are not unique",
+                ],
+                ["Fallthrough - no change", "Fallthrough - no change"],
+            ];
+
+            tracebackErrors.map(args => {
                 const input = args[0],
                     result = args[1];
-                assert.equal(parseError(input), result);
+                assert.equal(extractErrorFromTraceback(input), result);
+            });
+        });
+    });
+
+    describe("parsePydanticError", function() {
+        it("extracts the error from a pydantic exception", function() {
+            const pydanticErrors = [
+                [
+                    [
+                        {
+                            loc: ["datasets", "remove"],
+                            msg: "Input should be a valid number",
+                        },
+                    ],
+                    ["datasets: Input should be a valid number"],
+                ],
+            ];
+
+            pydanticErrors.map(args => {
+                const input = args[0],
+                    result = args[1];
+                assert.allEqual(parsePydanticError(input), result);
             });
         });
     });
