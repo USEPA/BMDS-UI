d57da76b3ef761596c002f211c20e745c064a5c9
Andy Shapiro <shapiromatron@gmail.com>
2024-04-17 23:39:31 -0400

    param prior table

--- frontend/src/components/IndividualModel/ParameterPriorTable.js
+++ frontend/src/components/IndividualModel/ParameterPriorTable.js
@@ -4,76 +4,47 @@ import PropTypes from "prop-types";
 import React, {Component} from "react";
 
 import {getLabel} from "@/common";
+import Table from "@/components/common/Table";
 import {isFrequentist, priorTypeLabels} from "@/constants/outputConstants";
 import {ff} from "@/utils/formatters";
 
-const renderPriorRow = prior => {
-        return (
-            <tr key={prior.name}>
-                <td>{prior.name}</td>
-                <td>{getLabel(prior.type, priorTypeLabels)}</td>
-                <td>{ff(prior.initial_value)}</td>
-                <td>{ff(prior.stdev)}</td>
-                <td>{ff(prior.min_value)}</td>
-                <td>{ff(prior.max_value)}</td>
-            </tr>
-        );
+const getBayesianData = parameters => {
+        return {
+            headers: ["Name", "Type", "Initial Value", "Std. Dev.", "Min Value", "Max Value"],
+            rows: _.range(parameters.names.length).map(idx => [
+                parameters.names[idx],
+                getLabel(parameters.prior_type[idx], priorTypeLabels),
+                ff(parameters.prior_initial_value[idx]),
+                ff(parameters.prior_stdev[idx]),
+                ff(parameters.prior_min_value[idx]),
+                ff(parameters.prior_max_value[idx]),
+            ]),
+            tblClasses: "table table-sm table-bordered text-right col-l-1 col-l-2",
+            subheader: "Parameter Settings",
+        };
     },
-    renderFrequentistRow = prior => {
-        return (
-            <tr key={prior.name}>
-                <td>{prior.name}</td>
-                <td>{ff(prior.initial_value)}</td>
-                <td>{ff(prior.min_value)}</td>
-                <td>{ff(prior.max_value)}</td>
-            </tr>
-        );
+    getFrequentistData = parameters => {
+        return {
+            headers: ["Name", "Initial Value", "Min Value", "Max Value"],
+            rows: _.range(parameters.names.length).map(idx => [
+                parameters.names[idx],
+                ff(parameters.prior_initial_value[idx]),
+                ff(parameters.prior_min_value[idx]),
+                ff(parameters.prior_max_value[idx]),
+            ]),
+            tblClasses: "table table-sm table-bordered text-right col-l-1",
+            subheader: "Parameter Settings",
+        };
     };
 
 @observer
 class ParameterPriorTable extends Component {
     render() {
         const {parameters, priorClass} = this.props,
-            isFreq = isFrequentist(priorClass),
-            rowFunction = isFreq ? renderFrequentistRow : renderPriorRow,
-            rows = _.range(parameters.names.length).map(idx => {
-                return {
-                    name: parameters.names[idx],
-                    type: parameters.prior_type[idx],
-                    initial_value: parameters.prior_initial_value[idx],
-                    stdev: parameters.prior_stdev[idx],
-                    min_value: parameters.prior_min_value[idx],
-                    max_value: parameters.prior_max_value[idx],
-                };
-            });
-
-        return (
-            <table className="table table-sm table-bordered text-right col-l-1">
-                <thead>
-                    <tr className="bg-custom">
-                        <th colSpan={isFreq ? 4 : 6}>Parameter Settings</th>
-                    </tr>
-                    {isFreq ? (
-                        <tr>
-                            <th>Name</th>
-                            <th>Initial Value</th>
-                            <th>Min Value</th>
-                            <th>Max Value</th>
-                        </tr>
-                    ) : (
-                        <tr>
-                            <th>Name</th>
-                            <th>Type</th>
-                            <th>Initial Value</th>
-                            <th>Std. Dev.</th>
-                            <th>Min Value</th>
-                            <th>Max Value</th>
-                        </tr>
-                    )}
-                </thead>
-                <tbody>{rows.map(rowFunction)}</tbody>
-            </table>
-        );
+            data = isFrequentist(priorClass)
+                ? getFrequentistData(parameters)
+                : getBayesianData(parameters);
+        return <Table data={data} />;
     }
 }
 
