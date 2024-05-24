6c176b06b2ecc4db42b36e06921af68241c3a717
Andy Shapiro <shapiromatron@gmail.com>
2024-05-23 20:14:26 -0400

    add worker healthcheck tests

--- bmds_server/common/api.py
+++ bmds_server/common/api.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 
-from .renderers import SvgRenderer
 from .worker_health import worker_healthcheck
 
 
@@ -21,20 +20,6 @@ class HealthcheckViewset(viewsets.ViewSet):
         is_healthy = worker_healthcheck.healthy()
         status_code = status.HTTP_200_OK if is_healthy else status.HTTP_503_SERVICE_UNAVAILABLE
         return Response({"healthy": is_healthy}, status=status_code)
-
-    @action(
-        detail=False,
-        url_path="worker-plot",
-        renderer_classes=(SvgRenderer,),
-        permission_classes=(permissions.IsAdminUser,),
-    )
-    def worker_plot(self, request):
-        ax = worker_healthcheck.plot()
-        return Response(ax)
-
-    @action(detail=False, url_path="worker-stats", permission_classes=(permissions.IsAdminUser,))
-    def worker_stats(self, request):
-        return Response(worker_healthcheck.stats())
 
     @action(
         detail=False,
