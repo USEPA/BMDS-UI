171f8edb4beb15700e9c539f29be2b5a3d76ad6f
Andy Shapiro <shapiromatron@gmail.com>
2024-05-28 10:18:49 -0400

    add tests

---
+++ tests/desktop/test_actions.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+import pytest
+from packaging.version import Version
+
+from bmds_server.desktop import actions
+
+
+@pytest.mark.vcr
+def test_get_latest_version():
+    uploaded, version = actions.get_latest_version("bmds")
+    assert isinstance(uploaded, datetime)
+    assert isinstance(version, Version)
+
+
+def test_get_installed_version():
+    version = actions.get_installed_version()
+    assert isinstance(version, Version)
+
+
+def test_get_version_message():
+    latest_date = datetime.strptime("2024-05-28", "%Y-%m-%d")
+    current = Version("2.0.0")
+    latest = Version("2.0.0")
+
+    resp = actions.get_version_message(current, latest, latest_date)
+    assert "You have the latest version installed" in resp
+
+    latest = Version("2.0.1")
+    resp = actions.get_version_message(current, latest, latest_date)
+    assert "There is a newer version available" in resp
+
+    latest = Version("1.0.0")
+    resp = actions.get_version_message(current, latest, latest_date)
+    assert "You have a newer version than what's currently available" in resp
