d7956ed72f4d0647725f508f8f3e0bf7f7db4896
Andy Shapiro <shapiromatron@gmail.com>
2024-07-16 17:57:37 -0400

    add new configuration parameter configuration

--- bmds_ui/desktop/cli.py
+++ bmds_ui/desktop/cli.py
@@ -1,13 +1,16 @@
 import argparse
 import os
+from pathlib import Path
 
 from .. import __version__
 from .app import BmdsDesktopTui
-from .config import Config
+from .config import Config, get_default_config_path
 from .log import setup_logging
 
 
-def get_app() -> BmdsDesktopTui:
+def get_app(config: str | None = None) -> BmdsDesktopTui:
+    if config:
+        os.environ["BMDS_CONFIG"] = str(Path(config).expanduser().resolve())
     setup_logging()
     os.environ["DJANGO_SETTINGS_MODULE"] = "bmds_ui.main.settings.desktop"
     Config.get()
@@ -20,9 +23,16 @@ def show_version():
 
 
 def main():
-    parser = argparse.ArgumentParser(description="BMDS Desktop Startup Interface")
+    parser = argparse.ArgumentParser(description=f"BMDS Desktop ({__version__})")
     parser.add_argument("--version", "-V", action="store_true", help="Show version")
+    parser.add_argument(
+        "--config",
+        metavar="config",
+        action="store",
+        help=f'Configuration path (Default: "{get_default_config_path()}")',
+        type=str,
+    )
     args = parser.parse_args()
     if args.version:
         return show_version()
-    get_app().run()
+    get_app(config=args.config).run()
