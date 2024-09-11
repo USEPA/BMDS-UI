a8af48cc17184fffd9d94a48b8757c3de41d3fc2
Andy Shapiro <shapiro.andy@epa.gov>
2024-09-11 14:28:30 -0400

    cleanup test

--- tests/analysis/test_utils.py
+++ tests/analysis/test_utils.py
@@ -3,10 +3,11 @@ from bmds_ui.analysis import utils
 
 def test_get_citation(settings):
     initial = settings.IS_DESKTOP
+
     settings.IS_DESKTOP = False
     assert "BMDS Online" in utils.get_citation()
-    print(utils.get_citation())
+
     settings.IS_DESKTOP = True
     assert "BMDS Desktop" in utils.get_citation()
-    print(utils.get_citation())
+
     settings.IS_DESKTOP = initial
