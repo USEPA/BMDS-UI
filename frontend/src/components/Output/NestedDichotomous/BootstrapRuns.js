2b945a75c47e6c981842cc16ecb423a23ff5a080
Andy Shapiro <shapiro.andy@epa.gov>
2024-11-15 16:16:49 -0500

    simplify ui

--- frontend/src/components/Output/NestedDichotomous/BootstrapRuns.js
+++ frontend/src/components/Output/NestedDichotomous/BootstrapRuns.js
@@ -9,37 +9,14 @@ import {ff, fractionalFormatter} from "@/utils/formatters";
 const getData = function(model) {
     const results = model.results.bootstrap,
         finalIdx = results.p_value.length - 1,
-        rows = _.range(results.p_value.length).map(idx => {
-            return idx === finalIdx
-                ? [
-                      <span key={0} className="font-weight-bold">
-                          Combined
-                      </span>,
-                      <span key={1} className="font-weight-bold">
-                          {fractionalFormatter(results.p_value[idx])}
-                      </span>,
-                      <span key={2} className="font-weight-bold">
-                          {ff(results.p50[idx])}
-                      </span>,
-                      <span key={3} className="font-weight-bold">
-                          {ff(results.p90[idx])}
-                      </span>,
-                      <span key={4} className="font-weight-bold">
-                          {ff(results.p95[idx])}
-                      </span>,
-                      <span key={5} className="font-weight-bold">
-                          {ff(results.p99[idx])}
-                      </span>,
-                  ]
-                : [
-                      idx + 1,
-                      fractionalFormatter(results.p_value[idx]),
-                      ff(results.p50[idx]),
-                      ff(results.p90[idx]),
-                      ff(results.p95[idx]),
-                      ff(results.p99[idx]),
-                  ];
-        });
+        rows = _.range(results.p_value.length).map(idx => [
+            idx === finalIdx ? "Combined" : idx + 1,
+            fractionalFormatter(results.p_value[idx]),
+            ff(results.p50[idx]),
+            ff(results.p90[idx]),
+            ff(results.p95[idx]),
+            ff(results.p99[idx]),
+        ]);
 
     return {
         headers: [
