9fb9879e54cfb1c833902cbdd7604e1b1e0b4a2c
Andy Shapiro <shapiromatron@gmail.com>
2025-01-19 19:14:30 -0500

    stub out migrator

--- bmds_ui/analysis/schema.py
+++ bmds_ui/analysis/schema.py
@@ -1,14 +1,21 @@
+import logging
 import re
+from copy import deepcopy
+from datetime import datetime
 from io import StringIO
+from typing import ClassVar
+from uuid import UUID
 
 import pandas as pd
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, ValidationError, field_validator
 from rest_framework.schemas.openapi import SchemaGenerator
 
 from pybmds.datasets.transforms.polyk import PolyKAdjustment
 from pybmds.types.session import VersionSchema
 
 from .validators import AnalysisSelectedSchema
+
+logger = logging.getLogger(__name__)
 
 
 class EditKeySchema(BaseModel):
@@ -96,3 +103,73 @@ def add_schema_to_path(schema: dict, path: str, verb: str, name: str):
 
 class ApiSchemaGenerator(SchemaGenerator):
     pass
+
+
+class Analysis(BaseModel):
+    id: UUID
+    inputs: dict
+    errors: list[dict]
+    outputs: AnalysisOutput
+    collections: list
+    is_executing: bool
+    is_finished: bool
+    has_errors: bool
+    inputs_valid: bool
+    api_url: str
+    excel_url: str
+    word_url: str
+    created: datetime
+    started: datetime
+    ended: datetime
+    starred: bool
+
+
+class AnalysisMigration(BaseModel):
+    initial: dict
+    initial_version: str
+    analysis: Analysis
+    version: str
+
+
+class SchemaMigrationException(Exception):
+    pass
+
+
+class AnalysisMigrator:
+    migration_chain: ClassVar = ["1.0", "1.1"]
+
+    @classmethod
+    def migrate(cls, data: dict) -> AnalysisMigration:
+        if not isinstance(data, dict):
+            raise SchemaMigrationException("Data must be a dictionary")
+
+        initial_version = data.get("outputs", {}).get("analysis_schema_version")
+        if initial_version is None or initial_version not in cls.migration_chain:
+            raise SchemaMigrationException("Cannot migrate; invalid version")
+
+        initial = deepcopy(data)
+        initial_index = cls.migration_chain.index(initial_version)
+        for idx in range(initial_index + 1, len(cls.migration_chain)):
+            migration = cls.migration_chain[idx]
+            func = f"to_{migration.replace('.', '_')}"
+            try:
+                data = getattr(cls, func)(data)
+            except Exception as err:
+                raise SchemaMigrationException("Cannot migrate; invalid data") from err
+
+        try:
+            analysis = Analysis.model_validate(data)
+        except ValidationError as err:
+            raise SchemaMigrationException("Cannot migrate; invalid data") from err
+
+        return AnalysisMigration(
+            initial=initial,
+            initial_version=initial_version,
+            analysis=analysis,
+            version=analysis.outputs.analysis_schema_version,
+        )
+
+    @classmethod
+    def to_1_1(cls, data: dict) -> dict:
+        logger.debug("Migrating from 1.0 to 1.1")
+        return data
