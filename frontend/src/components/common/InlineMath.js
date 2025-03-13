0b937d205a0c08b7c1e70b6091a056c9cdae75c9
Andy Shapiro <shapiro.andy@epa.gov>
2025-03-12 21:34:58 -0400

    add mathjax and link into django templates

---
+++ frontend/src/components/common/InlineMath.js
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+import React, {Component} from "react";
+
+class InlineMath extends Component {
+    render() {
+        const {f} = this.props;
+        return <span dangerouslySetInnerHTML={{__html: `$${f}$`}} />;
+    }
+}
+InlineMath.propTypes = {
+    f: PropTypes.string.isRequired,
+};
+
+export const typesetMath = function() {
+    if (window.MathJax) {
+        window.MathJax.typesetPromise();
+    }
+};
+export default InlineMath;
