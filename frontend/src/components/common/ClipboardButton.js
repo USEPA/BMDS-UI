3abdad6f3ee12cb12e0a5211fb29eec5cc497e79
Safia Rizwan <rizwan.safia@epa.gov>
2026-04-08 11:05:59 -0400

    Fixed clipboard button on-hover formatting

--- frontend/src/components/common/ClipboardButton.js
+++ frontend/src/components/common/ClipboardButton.js
@@ -1,47 +1,50 @@
 import Clipboard from "clipboard";
 import PropTypes from "prop-types";
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import Icon from "./Icon";
 
 class ClipboardButton extends Component {
-    // copy text to clipboard
-    constructor(props) {
-        super(props);
-        this.ref = React.createRef();
+  // copy text to clipboard
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+  componentDidMount() {
+    const { textToCopy, onCopy } = this.props;
+    this.clipboardEvent = new Clipboard(this.ref.current, {
+      text: () => textToCopy,
+    });
+    if (onCopy) {
+      this.clipboardEvent.on("success", onCopy);
     }
-    componentDidMount() {
-        const {textToCopy, onCopy} = this.props;
-        this.clipboardEvent = new Clipboard(this.ref.current, {text: () => textToCopy});
-        if (onCopy) {
-            this.clipboardEvent.on("success", onCopy);
-        }
-    }
-    componentWillUnmount() {
-        this.clipboardEvent.destroy();
-    }
-    render() {
-        const {className, textToCopy, text} = this.props;
-        return (
-            <button
-                ref={this.ref}
-                type="button"
-                className={className}
-                title={`Copy "${textToCopy}" to clipboard`}>
-                <Icon name="clipboard" text={text} />
-            </button>
-        );
-    }
+  }
+  componentWillUnmount() {
+    this.clipboardEvent.destroy();
+  }
+  render() {
+    const { className, textToCopy, text } = this.props;
+    return (
+      <button
+        ref={this.ref}
+        type="button"
+        className={className}
+        title={`Copy to clipboard: \n${textToCopy}`}
+      >
+        <Icon name="clipboard" text={text} />
+      </button>
+    );
+  }
 }
 
 ClipboardButton.propTypes = {
-    text: PropTypes.string.isRequired,
-    textToCopy: PropTypes.string.isRequired,
-    className: PropTypes.string.isRequired,
-    onCopy: PropTypes.func,
+  text: PropTypes.string.isRequired,
+  textToCopy: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  onCopy: PropTypes.func,
 };
 ClipboardButton.defaultProps = {
-    text: "",
+  text: "",
 };
 
 export default ClipboardButton;
