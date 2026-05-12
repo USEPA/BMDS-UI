ca5e0d20f0e981404f4cb2631d42a5787eb6e3d3
Safia Rizwan <shajarizwan@gmail.com>
2026-05-12 12:25:54 -0400

    distribution type removed from LOUD continuous options settings

--- frontend/src/components/Main/OptionsForm/OptionsForm.js
+++ frontend/src/components/Main/OptionsForm/OptionsForm.js
@@ -3,10 +3,10 @@ import React from "react";
 
 import * as mc from "@/constants/mainConstants";
 import {
-    continuousBmrOptions,
-    dichotomousBmrOptions,
-    distTypeOptions,
-    litterSpecificCovariateOptions,
+  continuousBmrOptions,
+  dichotomousBmrOptions,
+  distTypeOptions,
+  litterSpecificCovariateOptions,
 } from "@/constants/optionsConstants";
 
 import Button from "../../common/Button";
@@ -15,141 +15,147 @@ import FloatInput from "../../common/FloatInput";
 import IntegerInput from "../../common/IntegerInput";
 import SelectInput from "../../common/SelectInput";
 
-const OptionsForm = props => {
-    return (
-        <tr className="form-group">
-            <td>{props.idx + 1}</td>
-            {props.modelType === mc.MODEL_CONTINUOUS ? (
-                <td>
-                    <SelectInput
-                        choices={continuousBmrOptions.map(option => {
-                            return {value: option.value, text: option.label};
-                        })}
-                        onChange={value =>
-                            props.saveOptions("bmr_type", parseInt(value), props.idx)
-                        }
-                        value={props.options.bmr_type}
-                    />
-                </td>
-            ) : null}
-            {props.modelType == mc.MODEL_DICHOTOMOUS ||
-            props.modelType == mc.MODEL_NESTED_DICHOTOMOUS ||
-            props.modelType == mc.MODEL_MULTI_TUMOR ? (
-                <td>
-                    <SelectInput
-                        choices={dichotomousBmrOptions.map(option => {
-                            return {value: option.value, text: option.label};
-                        })}
-                        onChange={value =>
-                            props.saveOptions("bmr_type", parseInt(value), props.idx)
-                        }
-                        value={props.options.bmr_type}
-                    />
-                </td>
-            ) : null}
+const OptionsForm = (props) => {
+  return (
+    <tr className="form-group">
+      <td>{props.idx + 1}</td>
+      {props.modelType === mc.MODEL_CONTINUOUS ? (
+        <td>
+          <SelectInput
+            choices={continuousBmrOptions.map((option) => {
+              return { value: option.value, text: option.label };
+            })}
+            onChange={(value) =>
+              props.saveOptions("bmr_type", parseInt(value), props.idx)
+            }
+            value={props.options.bmr_type}
+          />
+        </td>
+      ) : null}
+      {props.modelType == mc.MODEL_DICHOTOMOUS ||
+      props.modelType == mc.MODEL_NESTED_DICHOTOMOUS ||
+      props.modelType == mc.MODEL_MULTI_TUMOR ? (
+        <td>
+          <SelectInput
+            choices={dichotomousBmrOptions.map((option) => {
+              return { value: option.value, text: option.label };
+            })}
+            onChange={(value) =>
+              props.saveOptions("bmr_type", parseInt(value), props.idx)
+            }
+            value={props.options.bmr_type}
+          />
+        </td>
+      ) : null}
 
-            <td>
-                <FloatInput
-                    onChange={value => props.saveOptions("bmr_value", value, props.idx)}
-                    value={props.options.bmr_value}
-                />
-            </td>
-            {props.modelType === mc.MODEL_CONTINUOUS ? (
-                <td>
-                    <FloatInput
-                        value={props.options.tail_probability}
-                        onChange={value => props.saveOptions("tail_probability", value, props.idx)}
-                    />
-                </td>
-            ) : null}
-            <td>
-                <FloatInput
-                    value={props.options.confidence_level}
-                    onChange={value => props.saveOptions("confidence_level", value, props.idx)}
-                />
-            </td>
-            {props.modelType === mc.MODEL_CONTINUOUS ? (
-                <td>
-                    <SelectInput
-                        choices={distTypeOptions.map(option => {
-                            return {value: option.value, text: option.label};
-                        })}
-                        onChange={value =>
-                            props.saveOptions("dist_type", parseInt(value), props.idx)
-                        }
-                        value={props.options.dist_type}
-                    />
-                </td>
-            ) : null}
-            {props.modelType === mc.MODEL_NESTED_DICHOTOMOUS ? (
-                <>
-                    <td>
-                        <SelectInput
-                            value={props.options.litter_specific_covariate}
-                            onChange={value =>
-                                props.saveOptions(
-                                    "litter_specific_covariate",
-                                    parseInt(value),
-                                    props.idx
-                                )
-                            }
-                            choices={litterSpecificCovariateOptions
-                                .filter(d => d.value > 0)
-                                .map(option => {
-                                    return {value: option.value, text: option.label};
-                                })}
-                        />
-                    </td>
-                    <td>
-                        <CheckboxInput
-                            checked={props.options.estimate_background}
-                            onChange={value =>
-                                props.saveOptions("estimate_background", value, props.idx)
-                            }
-                        />
-                    </td>
-                    <td>
-                        <IntegerInput
-                            value={props.options.bootstrap_iterations}
-                            onChange={value =>
-                                props.saveOptions("bootstrap_iterations", value, props.idx)
-                            }
-                        />
-                    </td>
-                    <td>
-                        <IntegerInput
-                            value={props.options.bootstrap_seed}
-                            onChange={value =>
-                                props.saveOptions("bootstrap_seed", value, props.idx)
-                            }
-                        />
-                    </td>
-                </>
-            ) : null}
-            <td>
-                <Button
-                    className="btn btn-danger"
-                    title="Delete Option Set"
-                    onClick={_e => props.deleteOptions(props.idx)}
-                    icon="trash3-fill"
-                />
-            </td>
-        </tr>
-    );
+      <td>
+        <FloatInput
+          onChange={(value) => props.saveOptions("bmr_value", value, props.idx)}
+          value={props.options.bmr_value}
+        />
+      </td>
+      {props.modelType === mc.MODEL_CONTINUOUS ? (
+        <td>
+          <FloatInput
+            value={props.options.tail_probability}
+            onChange={(value) =>
+              props.saveOptions("tail_probability", value, props.idx)
+            }
+          />
+        </td>
+      ) : null}
+      <td>
+        <FloatInput
+          value={props.options.confidence_level}
+          onChange={(value) =>
+            props.saveOptions("confidence_level", value, props.idx)
+          }
+        />
+      </td>
+      {props.modelType === mc.MODEL_CONTINUOUS ? (
+        props.activeTab !== "loud_bayesian" ? (
+          <td>
+            <SelectInput
+              choices={distTypeOptions.map((option) => {
+                return { value: option.value, text: option.label };
+              })}
+              onChange={(value) =>
+                props.saveOptions("dist_type", parseInt(value), props.idx)
+              }
+              value={props.options.dist_type}
+            />
+          </td>
+        ) : null
+      ) : null}
+      {props.modelType === mc.MODEL_NESTED_DICHOTOMOUS ? (
+        <>
+          <td>
+            <SelectInput
+              value={props.options.litter_specific_covariate}
+              onChange={(value) =>
+                props.saveOptions(
+                  "litter_specific_covariate",
+                  parseInt(value),
+                  props.idx,
+                )
+              }
+              choices={litterSpecificCovariateOptions
+                .filter((d) => d.value > 0)
+                .map((option) => {
+                  return { value: option.value, text: option.label };
+                })}
+            />
+          </td>
+          <td>
+            <CheckboxInput
+              checked={props.options.estimate_background}
+              onChange={(value) =>
+                props.saveOptions("estimate_background", value, props.idx)
+              }
+            />
+          </td>
+          <td>
+            <IntegerInput
+              value={props.options.bootstrap_iterations}
+              onChange={(value) =>
+                props.saveOptions("bootstrap_iterations", value, props.idx)
+              }
+            />
+          </td>
+          <td>
+            <IntegerInput
+              value={props.options.bootstrap_seed}
+              onChange={(value) =>
+                props.saveOptions("bootstrap_seed", value, props.idx)
+              }
+            />
+          </td>
+        </>
+      ) : null}
+      <td>
+        <Button
+          className="btn btn-danger"
+          title="Delete Option Set"
+          onClick={(_e) => props.deleteOptions(props.idx)}
+          icon="trash3-fill"
+        />
+      </td>
+    </tr>
+  );
 };
 
 OptionsForm.propTypes = {
-    optionsStore: PropTypes.string,
-    idx: PropTypes.number.isRequired,
-    modelType: PropTypes.string.isRequired,
-    onChange: PropTypes.func,
-    saveOptions: PropTypes.func.isRequired,
-    deleteOptions: PropTypes.func.isRequired,
-    options: PropTypes.object.isRequired,
-    bmr_value: PropTypes.number,
-    tail_probability: PropTypes.number,
-    distribution: PropTypes.string,
-    variance: PropTypes.string,
-    delete: PropTypes.func,
+  optionsStore: PropTypes.string,
+  idx: PropTypes.number.isRequired,
+  modelType: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  saveOptions: PropTypes.func.isRequired,
+  deleteOptions: PropTypes.func.isRequired,
+  options: PropTypes.object.isRequired,
+  bmr_value: PropTypes.number,
+  tail_probability: PropTypes.number,
+  distribution: PropTypes.string,
+  variance: PropTypes.string,
+  delete: PropTypes.func,
 };
 export default OptionsForm;
--- frontend/src/components/Main/OptionsForm/OptionsFormList.js
+++ frontend/src/components/Main/OptionsForm/OptionsFormList.js
@@ -1,13 +1,13 @@
-import {toJS} from "mobx";
-import {inject, observer} from "mobx-react";
+import { toJS } from "mobx";
+import { inject, observer } from "mobx-react";
 import PropTypes from "prop-types";
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import {
-    MODEL_CONTINUOUS,
-    MODEL_DICHOTOMOUS,
-    MODEL_MULTI_TUMOR,
-    MODEL_NESTED_DICHOTOMOUS,
+  MODEL_CONTINUOUS,
+  MODEL_DICHOTOMOUS,
+  MODEL_MULTI_TUMOR,
+  MODEL_NESTED_DICHOTOMOUS,
 } from "@/constants/mainConstants";
 
 import Button from "../../common/Button";
@@ -15,123 +15,128 @@ import HelpTextPopover from "../../common/HelpTextPopover";
 import OptionsForm from "./OptionsForm";
 import OptionsReadOnly from "./OptionsReadOnly";
 
-@inject("optionsStore")
+@inject(({ optionsStore, modelsStore }) => ({
+  optionsStore,
+  activeTab: modelsStore.activeTab,
+}))
 @observer
 class OptionsFormList extends Component {
-    render() {
-        const {optionsStore} = this.props,
-            modelType = optionsStore.getModelType,
-            optionsList = toJS(optionsStore.optionsList),
-            distTypeHelpText =
-                "If lognormal is selected, only the Exponential and Hill models can be executed. Other models will be removed during the execution process and will not be shown in the outputs.",
-            tailProbabilityHelpText = "Only used for Hybrid models.";
-        return (
-            <div>
-                <div className="panel panel-default">
-                    <form className="form-horizontal">
-                        <table className="table table-sm table-fixed">
-                            <thead className="bg-custom">
-                                <tr>
-                                    <th>Option Set #</th>
-                                    {modelType === MODEL_CONTINUOUS ? (
-                                        <>
-                                            <th>BMR Type</th>
-                                            <th>BMRF</th>
-                                            <th>
-                                                Tail Probability
-                                                <HelpTextPopover
-                                                    content={tailProbabilityHelpText}
-                                                />
-                                            </th>
-                                            <th>Confidence Level (one sided)</th>
-                                            <th>
-                                                Distribution +<br />
-                                                Variance&nbsp;
-                                                <HelpTextPopover content={distTypeHelpText} />
-                                            </th>
-                                        </>
-                                    ) : null}
-                                    {modelType === MODEL_DICHOTOMOUS ||
-                                    modelType === MODEL_MULTI_TUMOR ? (
-                                        <>
-                                            <th>Risk Type</th>
-                                            <th>BMR</th>
-                                            <th>Confidence Level (one sided)</th>
-                                        </>
-                                    ) : null}
-                                    {modelType === MODEL_NESTED_DICHOTOMOUS ? (
-                                        <>
-                                            <th>Risk Type</th>
-                                            <th>BMR</th>
-                                            <th>Confidence Level (one sided)</th>
-                                            <th>
-                                                Litter Specific
-                                                <br />
-                                                Covariate
-                                            </th>
-                                            <th>Estimate Background</th>
-                                            <th>
-                                                Bootstrap
-                                                <br />
-                                                Iterations
-                                            </th>
-                                            <th>Bootstrap Seed</th>
-                                        </>
-                                    ) : null}
-                                    {optionsStore.canEdit ? (
-                                        <th style={{width: "55px"}}>
-                                            <Button
-                                                title="Add option set."
-                                                className="btn btn-primary"
-                                                disabled={!optionsStore.canAddNewOption}
-                                                onClick={optionsStore.addOptions}
-                                                icon="plus-circle"
-                                            />
-                                        </th>
-                                    ) : null}
-                                </tr>
-                            </thead>
-                            {optionsStore.canEdit ? (
-                                <tbody>
-                                    {optionsList.map((options, id) => (
-                                        <OptionsForm
-                                            key={id}
-                                            idx={id}
-                                            options={options}
-                                            modelType={modelType}
-                                            deleteOptions={optionsStore.deleteOptions}
-                                            saveOptions={optionsStore.saveOptions}
-                                        />
-                                    ))}
-                                </tbody>
-                            ) : (
-                                <tbody>
-                                    {optionsList.map((options, id) => (
-                                        <OptionsReadOnly
-                                            key={id}
-                                            idx={id}
-                                            options={options}
-                                            modelType={modelType}
-                                        />
-                                    ))}
-                                </tbody>
-                            )}
-                        </table>
-                        {optionsStore.canAddNewOption ? null : (
-                            <p className="text-danger">
-                                Can have a maximum of {optionsStore.maxItems} option sets per
-                                analysis.
-                            </p>
-                        )}
-                    </form>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { optionsStore, activeTab } = this.props,
+      modelType = optionsStore.getModelType,
+      optionsList = toJS(optionsStore.optionsList),
+      distTypeHelpText =
+        "If lognormal is selected, only the Exponential and Hill models can be executed. Other models will be removed during the execution process and will not be shown in the outputs.",
+      tailProbabilityHelpText = "Only used for Hybrid models.";
+    return (
+      <div>
+        <div className="panel panel-default">
+          <form className="form-horizontal">
+            <table className="table table-sm table-fixed">
+              <thead className="bg-custom">
+                <tr>
+                  <th>Option Set #</th>
+                  {modelType === MODEL_CONTINUOUS ? (
+                    <>
+                      <th>BMR Type</th>
+                      <th>BMRF</th>
+                      <th>
+                        Tail Probability
+                        <HelpTextPopover content={tailProbabilityHelpText} />
+                      </th>
+                      <th>Confidence Level (one sided)</th>
+                      {activeTab !== "loud_bayesian" && (
+                        <th>
+                          Distribution +<br />
+                          Variance&nbsp;
+                          <HelpTextPopover content={distTypeHelpText} />
+                        </th>
+                      )}
+                    </>
+                  ) : null}
+                  {modelType === MODEL_DICHOTOMOUS ||
+                  modelType === MODEL_MULTI_TUMOR ? (
+                    <>
+                      <th>Risk Type</th>
+                      <th>BMR</th>
+                      <th>Confidence Level (one sided)</th>
+                    </>
+                  ) : null}
+                  {modelType === MODEL_NESTED_DICHOTOMOUS ? (
+                    <>
+                      <th>Risk Type</th>
+                      <th>BMR</th>
+                      <th>Confidence Level (one sided)</th>
+                      <th>
+                        Litter Specific
+                        <br />
+                        Covariate
+                      </th>
+                      <th>Estimate Background</th>
+                      <th>
+                        Bootstrap
+                        <br />
+                        Iterations
+                      </th>
+                      <th>Bootstrap Seed</th>
+                    </>
+                  ) : null}
+                  {optionsStore.canEdit ? (
+                    <th style={{ width: "55px" }}>
+                      <Button
+                        title="Add option set."
+                        className="btn btn-primary"
+                        disabled={!optionsStore.canAddNewOption}
+                        onClick={optionsStore.addOptions}
+                        icon="plus-circle"
+                      />
+                    </th>
+                  ) : null}
+                </tr>
+              </thead>
+              {optionsStore.canEdit ? (
+                <tbody>
+                  {optionsList.map((options, id) => (
+                    <OptionsForm
+                      key={id}
+                      idx={id}
+                      options={options}
+                      modelType={modelType}
+                      deleteOptions={optionsStore.deleteOptions}
+                      saveOptions={optionsStore.saveOptions}
+                      activeTab={activeTab}
+                    />
+                  ))}
+                </tbody>
+              ) : (
+                <tbody>
+                  {optionsList.map((options, id) => (
+                    <OptionsReadOnly
+                      key={id}
+                      idx={id}
+                      options={options}
+                      modelType={modelType}
+                    />
+                  ))}
+                </tbody>
+              )}
+            </table>
+            {optionsStore.canAddNewOption ? null : (
+              <p className="text-danger">
+                Can have a maximum of {optionsStore.maxItems} option sets per
+                analysis.
+              </p>
+            )}
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 OptionsFormList.propTypes = {
-    optionsStore: PropTypes.object,
+  optionsStore: PropTypes.object,
+  activeTab: PropTypes.string,
 };
 
 export default OptionsFormList;
